82d382bdd053ebd80a09dd65b05fe219e5ce8c2f
mhdzumair <mhdzumair@gmail.com>
2023-09-11 02:00:17 +0530

    fix stream host url issue

--- db/config.py
+++ db/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     mongo_uri: str
     git_rev: str = "beta"
     secret_key: str
+    host_url: str = "https://882b9915d0fe-stremio-tamilblasters.baby-beamup.club"
 
     class Config:
         env_file = ".env"
