ecfed4aadd68a4b4a7c5817ca3e6771457eb6d95
mhdzumair <mhdzumair@gmail.com>
2024-02-12 02:09:18 +0530

    Add get_client_ip function to extract client's real IP address

--- api/middleware.py
+++ api/middleware.py
@@ -4,6 +4,23 @@ from fastapi.requests import Request
 from fastapi.responses import Response
 
 
+def get_client_ip(request: Request) -> str | None:
+    """
+    Extract the client's real IP address from the request headers or fallback to the client host.
+    """
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+    if x_forwarded_for:
+        # In some cases, this header can contain multiple IPs
+        # separated by commas.
+        # The first one is the original client's IP.
+        return x_forwarded_for.split(",")[0].strip()
+    # Fallback to X-Real-IP if X-Forwarded-For is not available
+    x_real_ip = request.headers.get("X-Real-IP")
+    if x_real_ip:
+        return x_real_ip
+    return request.client.host if request.client else "Unknown"
+
+
 class SecureLoggingMiddleware:
     async def __call__(self, request: Request, call_next):
         response = await call_next(request)
@@ -12,12 +29,12 @@ class SecureLoggingMiddleware:
 
     @staticmethod
     def custom_log(request: Request, response: Response):
-        client_host = request.client.host if request.client else "unknown"
+        ip = get_client_ip(request)
         url_path = str(request.url)
         if request.path_params.get("secret_str"):
             url_path = url_path.replace(
                 request.path_params.get("secret_str"), "***MASKED***"
             )
         logging.info(
-            f'{client_host} - "{request.method} {url_path} HTTP/1.1" {response.status_code}'
+            f'{ip} - "{request.method} {url_path} HTTP/1.1" {response.status_code}'
         )
