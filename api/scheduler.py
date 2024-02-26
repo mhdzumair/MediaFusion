f723c1df344f31b1bb77fb9b775359b8d38d98d8
mhdzumair <mhdzumair@gmail.com>
2024-02-27 00:55:51 +0530

    Add formula_tgx scraper to docker-compose scheduler

--- api/scheduler.py
+++ api/scheduler.py
@@ -3,6 +3,8 @@ import logging
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 from scrapers import tamil_blasters, tamilmv
 
@@ -10,6 +12,16 @@ from scrapers import tamil_blasters, tamilmv
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+
+def run_formula_tgx_spider():
+    """
+    Function to start the formula_tgx spider.
+    """
+    process = CrawlerProcess(get_project_settings())
+    process.crawl("formula_tgx")  # Use the spider name you have defined
+    process.start()
+
 
 scheduler = AsyncIOScheduler()
 
@@ -22,6 +34,9 @@ scheduler.add_job(
 
 # Setup tamilmv scraper
 scheduler.add_job(tamilmv.run_schedule_scrape, CronTrigger(hour="*/3"), name="tamilmv")
+
+# Setup formula_tgx scraper
+scheduler.add_job(run_formula_tgx_spider, CronTrigger(hour="*/12"), name="formula_tgx")
 
 # Start the scheduler
 scheduler.start()
