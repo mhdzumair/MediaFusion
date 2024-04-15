b4b91d4b348b23b890c09bc589ea0f58b626078b
mhdzumair <mhdzumair@gmail.com>
2024-04-15 18:49:38 +0530

    bugfix schedulers

--- api/scheduler.py
+++ api/scheduler.py
@@ -17,7 +17,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         return
 
     # Setup tamil blasters scraper
-    if not settings.disable_tamil_blasters_scheduler:
+    if settings.disable_tamil_blasters_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.tamil_blasters_scheduler_crontab),
@@ -26,7 +26,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Setup tamilmv scraper
-    if not settings.disable_tamilmv_scheduler:
+    if settings.disable_tamilmv_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.tamilmv_scheduler_crontab),
@@ -35,7 +35,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Setup formula_tgx scraper
-    if not settings.disable_formula_tgx_scheduler:
+    if settings.disable_formula_tgx_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.formula_tgx_scheduler_crontab),
@@ -44,7 +44,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Setup mhdtvworld scraper
-    if not settings.disable_mhdtvworld_scheduler:
+    if settings.disable_mhdtvworld_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.mhdtvworld_scheduler_crontab),
@@ -53,7 +53,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Setup mhdtvsports scraper
-    if not settings.disable_mhdtvsports_scheduler:
+    if settings.disable_mhdtvsports_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.mhdtvsports_scheduler_crontab),
@@ -62,7 +62,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Setup tamilultra scraper
-    if not settings.disable_tamilultra_scheduler:
+    if settings.disable_tamilultra_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.tamilultra_scheduler_crontab),
@@ -71,7 +71,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Schedule validate_tv_streams_in_db
-    if not settings.disable_validate_tv_streams_in_db:
+    if settings.disable_validate_tv_streams_in_db:
         scheduler.add_job(
             validate_tv_streams_in_db.send,
             CronTrigger.from_crontab(settings.validate_tv_streams_in_db_crontab),
@@ -79,7 +79,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Schedule sport_video scraper
-    if not settings.disable_sport_video_scheduler:
+    if settings.disable_sport_video_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.sport_video_scheduler_crontab),
@@ -88,7 +88,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Schedule streamed scraper
-    if not settings.disable_streamed_scheduler:
+    if settings.disable_streamed_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.streamed_scheduler_crontab),
@@ -97,7 +97,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Schedule mrgamingstreams scraper
-    if not settings.disable_streamed_scheduler:
+    if settings.disable_mrgamingstreams_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.mrgamingstreams_scheduler_crontab),
@@ -106,7 +106,7 @@ def setup_scheduler(scheduler: AsyncIOScheduler):
         )
 
     # Schedule crictime scraper
-    if not settings.disable_crictime_scheduler:
+    if settings.disable_crictime_scheduler:
         scheduler.add_job(
             run_spider.send,
             CronTrigger.from_crontab(settings.crictime_scheduler_crontab),
