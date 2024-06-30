a2b69ef86e24311529e5d3bede6c6f634481d43d
mhdzumair <mhdzumair@gmail.com>
2024-06-30 19:15:46 +0530

    Kodi: validate pydevd server before calling trace function

--- kodi/plugin.video.mediafusion/main.py
+++ kodi/plugin.video.mediafusion/main.py
@@ -1,4 +1,5 @@
 import os
+import socket
 import sys
 
 import xbmc
@@ -8,9 +9,22 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 try:
     import pydevd_pycharm
 
-    pydevd_pycharm.settrace(
-        "localhost", port=8877, stdoutToServer=True, stderrToServer=True
-    )
+    # validate if pydevd_pycharm server is running
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(3)
+    result = sock.connect_ex(("localhost", 8877))
+    sock.close()
+    if result == 0:
+        xbmc.log("pydevd_pycharm server is running. start debugging", xbmc.LOGINFO)
+        pydevd_pycharm.settrace(
+            "localhost", port=8877, stdoutToServer=True, stderrToServer=True
+        )
+    else:
+        xbmc.log(
+            "pydevd_pycharm server is not running. not starting debug session",
+            xbmc.LOGINFO,
+        )
+
 except ImportError:
     xbmc.log("pydevd_pycharm not imported. not starting debug session", xbmc.LOGWARNING)
 except ConnectionRefusedError:
