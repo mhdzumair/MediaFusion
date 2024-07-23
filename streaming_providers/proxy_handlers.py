660efa210d60fa9824a173e8946f40acf50df0ad
mhdzumair <mhdzumair@gmail.com>
2024-07-23 22:17:16 +0530

    improved performance of streaming proxy

--- streaming_providers/proxy_handlers.py
+++ streaming_providers/proxy_handlers.py
@@ -1,6 +1,6 @@
-import aiohttp
-import httpx
 import logging
+
+import httpx
 from fastapi import Response
 from fastapi.responses import StreamingResponse
 from starlette.background import BackgroundTask
@@ -9,43 +9,44 @@ logger = logging.getLogger(__name__)
 
 
 async def handle_head_request(video_url: str) -> Response:
-    try:
-        async with aiohttp.ClientSession() as session, session.head(
-            video_url
-        ) as head_response:
+    async with httpx.AsyncClient() as client:
+        try:
+            head_response = await client.head(video_url)
             head_response.raise_for_status()
             return Response(
                 headers={
-                    "Content-Length": head_response.headers.get("Content-Length", ""),
+                    "Content-Length": head_response.headers.get("content-length", ""),
                     "Accept-Ranges": head_response.headers.get(
-                        "Accept-Ranges", "bytes"
+                        "accept-ranges", "bytes"
                     ),
                 },
-                status_code=head_response.status,
+                status_code=head_response.status_code,
             )
-    except aiohttp.ClientError as e:
-        logger.error(f"Upstream service error while handling HEAD request: {e}")
-        return Response(status_code=502, content=f"Upstream service error: {e}")
-    except Exception as e:
-        logger.error(f"Internal server error while handling HEAD request: {e}")
-        return Response(status_code=500, content=f"Internal server error: {e}")
+        except httpx.HTTPStatusError as e:
+            logger.error(f"Upstream service error while handling HEAD request: {e}")
+            return Response(status_code=502, content=f"Upstream service error: {e}")
+        except Exception as e:
+            logger.error(f"Internal server error while handling HEAD request: {e}")
+            return Response(status_code=500, content=f"Internal server error: {e}")
 
 
 class Streamer:
-    def __init__(self):
-        self.client = httpx.AsyncClient()
+    def __init__(self, client):
+        self.client = client
         self.response = None
 
-    async def stream_content(self, video_url: str, headers: dict):
+    async def stream_content(
+        self, video_url: str, headers: dict, chunk_size: int = 65536
+    ):
         try:
             async with self.client.stream(
                 "GET", video_url, headers=headers
             ) as self.response:
                 self.response.raise_for_status()
-                async for chunk in self.response.aiter_raw():
+                async for chunk in self.response.aiter_bytes(chunk_size):
                     yield chunk
         except httpx.HTTPStatusError as e:
-            logger.error(f"HTTPStatusError while streaming content: {e}")
+            logger.error(f"HTTP error while streaming content: {e}")
             raise e
         except Exception as e:
             logger.error(f"Unexpected error while streaming content: {e}")
@@ -55,7 +56,8 @@ class Streamer:
         try:
             if self.response is not None:
                 await self.response.aclose()
-            await self.client.aclose()
+            if self.client is not None:
+                await self.client.aclose()
         except Exception as e:
             logger.error(f"Error while closing Streamer: {e}")
 
@@ -63,31 +65,25 @@ class Streamer:
 async def handle_get_request(
     video_url: str, range_header: str
 ) -> StreamingResponse | Response:
+    client = httpx.AsyncClient()
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.head(
-                video_url, headers={"Range": range_header}
-            ) as head_response:
-                head_response.raise_for_status()
-                if head_response.status == 206:
-                    streamer = Streamer()
-
-                    return StreamingResponse(
-                        streamer.stream_content(video_url, {"Range": range_header}),
-                        status_code=206,
-                        headers={
-                            "Content-Range": head_response.headers["Content-Range"],
-                            "Content-Length": head_response.headers["Content-Length"],
-                            "Accept-Ranges": "bytes",
-                        },
-                        background=BackgroundTask(streamer.close),
-                    )
-    except aiohttp.ClientError as e:
+        head_response = await client.head(video_url, headers={"Range": range_header})
+        head_response.raise_for_status()
+        if head_response.status_code == 206:
+            streamer = Streamer(client)
+            return StreamingResponse(
+                streamer.stream_content(video_url, {"Range": range_header}),
+                status_code=206,
+                headers={
+                    "Content-Range": head_response.headers["content-range"],
+                    "Content-Length": head_response.headers["content-length"],
+                    "Accept-Ranges": "bytes",
+                },
+                background=BackgroundTask(streamer.close),
+            )
+    except httpx.HTTPStatusError as e:
         logger.error(f"Upstream service error while handling GET request: {e}")
         return Response(status_code=502, content=f"Upstream service error: {e}")
-    except httpx.HTTPStatusError as e:
-        logger.error(f"Streaming service error: {e}")
-        return Response(status_code=502, content=f"Streaming service error: {e}")
     except Exception as e:
         logger.error(f"Internal server error while handling GET request: {e}")
         return Response(status_code=500, content=f"Internal server error: {e}")
