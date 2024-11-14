904aebd6d358e80ba88a29253e2433fb1c11d1df
mhdzumair <mhdzumair@gmail.com>
2024-11-14 23:34:24 +0530

    Revise `is_video_file` function to support more formats
    Expanded the list of supported video file extensions in the `is_video_file` function to include modern streaming formats, high-efficiency containers, and legacy formats. Added docstring to provide a clear explanation of the function's purpose and usage.

--- utils/validation_helper.py
+++ utils/validation_helper.py
@@ -135,42 +135,97 @@ async def validate_tv_metadata(metadata: schemas.TVMetaData) -> list[schemas.TVS
 
 
 def is_video_file(filename: str) -> bool:
+    """
+    Fast check if a filename is a playable video format supported by
+    modern media players (VLC, MPV, etc.)
+
+    Covers all modern containers and formats including
+    - Modern streaming formats (HLS, DASH)
+    - High-efficiency containers (MKV, MP4, WebM)
+    - Professional formats that are commonly playable
+    - Network streaming formats
+
+    Args:
+        filename: URL or filename to check
+
+    Returns:
+        bool: True if the filename is a playable video format
+    """
     return filename.lower().endswith(
         (
-            ".3g2",
-            ".3gp",
-            ".amv",
-            ".asf",
-            ".avi",
-            ".drc",
-            ".flv",
-            ".gif",
-            ".gifv",
-            ".m2v",
-            ".m4p",
-            ".m4v",
-            ".mkv",
-            ".mng",
-            ".mov",
-            ".mp2",
-            ".mp4",
-            ".mpe",
-            ".mpeg",
-            ".mpg",
-            ".mpv",
-            ".mxf",
-            ".nsv",
-            ".ogg",
-            ".ogv",
-            ".qt",
-            ".rm",
-            ".rmvb",
-            ".roq",
-            ".svi",
-            ".vob",
-            ".webm",
-            ".wmv",
-            ".yuv",
+            # Modern Containers (most common first)
+            ".mp4",  # MPEG-4 Part 14
+            ".mkv",  # Matroska
+            ".webm",  # WebM
+            ".m4v",  # MPEG-4
+            ".mov",  # QuickTime
+            # Streaming Formats
+            ".m3u8",  # HLS
+            ".m3u",  # Playlist
+            ".mpd",  # DASH
+            # MPEG Transport Streams
+            ".ts",  # Transport Stream
+            ".mts",  # MPEG Transport Stream
+            ".m2ts",  # Blu-ray Transport Stream
+            ".m2t",  # MPEG-2 Transport Stream
+            # MPEG Program Streams
+            ".mpeg",  # MPEG Program Stream
+            ".mpg",  # MPEG Program Stream
+            ".mp2",  # MPEG Program Stream
+            ".m2v",  # MPEG-2 Video
+            ".m4p",  # Protected MPEG-4 Part 14
+            # Common Legacy Formats (still widely supported)
+            ".avi",  # Audio Video Interleave
+            ".wmv",  # Windows Media Video
+            ".flv",  # Flash Video
+            ".f4v",  # Flash MP4 Video
+            ".ogv",  # Ogg Video
+            ".ogm",  # Ogg Media
+            ".rm",  # RealMedia
+            ".rmvb",  # RealMedia Variable Bitrate
+            ".asf",  # Advanced Systems Format
+            ".divx",  # DivX Video
+            # Mobile Formats
+            ".3gp",  # 3GPP
+            ".3g2",  # 3GPP2
+            # DVD/Blu-ray Formats
+            ".vob",  # DVD Video Object
+            ".ifo",  # DVD Information
+            ".bdmv",  # Blu-ray Movie
+            # Modern High-Efficiency Formats
+            ".hevc",  # High Efficiency Video Coding
+            ".av1",  # AOMedia Video 1
+            ".vp8",  # WebM VP8
+            ".vp9",  # WebM VP9
+            # Additional Modern Formats
+            ".mxf",  # Material eXchange Format (broadcast)
+            ".dav",  # DVR365 Format
+            ".swf",  # Shockwave Flash (contains video)
+            # Network Streaming
+            ".nsv",  # Nullsoft Streaming Video
+            ".strm",  # Stream file
+            # Additional Container Formats
+            ".mvi",  # Motion Video Interface
+            ".vid",  # Generic video file
+            ".amv",  # Anime Music Video
+            ".m4s",  # MPEG-DASH Segment
+            ".mqv",  # Sony Movie Format
+            ".nuv",  # NuppelVideo
+            ".wtv",  # Windows Recorded TV Show
+            ".dvr-ms",  # Microsoft Digital Video Recording
+            # Playlist Formats
+            ".pls",  # Playlist File
+            ".cue",  # Cue Sheet
+            # Modern Streaming Service Formats
+            ".dash",  # DASH
+            ".hls",  # HLS Alternative
+            ".ismv",  # Smooth Streaming
+            ".m4f",  # Protected MPEG-4 Fragment
+            ".mp4v",  # MPEG-4 Video
+            # Animation Formats (playable in video players)
+            ".gif",  # Graphics Interchange Format
+            ".gifv",  # Imgur Video Alternative
+            ".apng",  # Animated PNG
         )
     )
 
