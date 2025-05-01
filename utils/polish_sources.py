33f4920bbdde8c68bc7fe3b57bace05c96aab5dd
mattjaas <183701493+mattjaas@users.noreply.github.com>
2025-05-01 14:27:08 +0200

    feat: detect Polish language for MULTi releases (groups & sites)

---
+++ utils/polish_sources.py
@@ -0,0 +1,39 @@
+"""
+Sources identifying Polish releases.
+- POLISH_RELEASE_GROUPS  – release-group tags (UPPERCASE, without spaces).
+- POLISH_SITES_NON_PL    – full hosts of sites whose domain ≠ '.pl'.
+  Use lowercase.
+"""
+
+POLISH_RELEASE_GROUPS: list[str] = [
+    "K83",
+    "P2P",
+    "BIRD",
+    "CHRISVPS",
+    "M80",
+    "DENDA",
+    "MR",
+    "R22",
+    "DSITE",
+    "RALF",
+    "BETON",
+    "KIT",
+    "KOSIARZ66",
+    "DREAM",
+    "LTN",
+    "MG",
+    "B89",
+    "COLO",
+    "KLIO",
+    "FOX",
+    "J60",
+    "LTS",
+    "ALUSIA",
+]
+
+POLISH_SITES_NON_PL: list[str] = [
+    "ex-torrenty.org",
+    "helltorrents.com",
+    "xtorrenty.org",
+    "polskie-torrenty.eu",
+]
