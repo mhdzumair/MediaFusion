d99baed28ff817e333f1f9de3c51f1a2f2e756ef
mhdzumair <mhdzumair@gmail.com>
2025-08-23 08:05:34 +0530

    Update TamilMV homepage URL and modify user agent string for scraping

--- mediafusion_scrapy/settings.py
+++ mediafusion_scrapy/settings.py
@@ -15,7 +15,7 @@ NEWSPIDER_MODULE = "mediafusion_scrapy.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Google Search Console)"
+USER_AGENT = "Mozilla/5.0 (Linux; Android 15; SM-S931B Build/AP3A.240905.015.A2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
