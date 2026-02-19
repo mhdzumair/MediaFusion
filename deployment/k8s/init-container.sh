6e80684cc0eeda53fcdc33522238ef9cb081f682
mhdzumair <mhdzumair@gmail.com>
2026-02-19 09:03:32 +0530

    remove flaresolverr health on init

--- deployment/k8s/init-container.sh
+++ deployment/k8s/init-container.sh
@@ -18,13 +18,4 @@ sed -i 's/\$PROWLARR__POSTGRES_HOST/'"$PROWLARR__POSTGRES_HOST"'/g' /config/conf
 sed -i 's/\$PROWLARR__POSTGRES_MAIN_DB/'"$PROWLARR__POSTGRES_MAIN_DB"'/g' /config/config.xml
 sed -i 's/\$PROWLARR__POSTGRES_LOG_DB/'"$PROWLARR__POSTGRES_LOG_DB"'/g' /config/config.xml
 chmod 664 /config/config.xml
-echo "Prowlarr config setup complete."
-
-# Check FlareSolverr health
-echo "Waiting for FlareSolverr to be ready..."
-until curl -s -o /dev/null -w "%{http_code}" "$FLARESOLVERR_HOST/health" | grep -q '^2'; do
-  echo "FlareSolverr is not ready. Retrying..."
-  sleep 5
-done
-
-echo "Everything is ready. Exiting init container."
+echo "Prowlarr config setup complete. Exiting init container."
